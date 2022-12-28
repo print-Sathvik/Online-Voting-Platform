f19eb3f3cef9529810859ed4b7f0f6e477a1d052
unknown <satvikreddy2017@gmail.com>
2022-12-28 21:20:59 +0530

    Imported all libraries and Created Admin Table

---
+++ app.js
@@ -0,0 +1,44 @@
+/* eslint-disable no-unused-vars */
+const express = require("express");
+var csrf = require("tiny-csrf");
+const app = express();
+const { Todo, User } = require("./models");
+const bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+
+const passport = require("passport");
+const connectEnsureLogin = require("connect-ensure-login");
+const session = require("express-session");
+const flash = require("connect-flash");
+const LocalStrategy = require("passport-local");
+const bcrypt = require("bcrypt");
+
+const saltRounds = 10;
+
+app.use(bodyParser.json());
+const path = require("path");
+// eslint-disable-next-line no-unused-vars
+const { response } = require("express");
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser("A secret string"));
+app.use(csrf("this_should_be_32_character_long", ["POST", "PUT", "DELETE"]));
+
+//Setting EJS as view engine
+app.set("view engine", "ejs");
+
+//Location of static html and CSS files to render our application
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
+
+app.use(
+  session({
+    secret: "secret-key for session 123abc",
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000, //in milli seconds
+    },
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
