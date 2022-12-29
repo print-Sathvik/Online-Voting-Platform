075ab3db9ca9b9e16a6b05e99e615dd34f01b649
unknown <satvikreddy2017@gmail.com>
2022-12-29 19:23:40 +0530

    2.Sign Up Successful

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require("express");
 var csrf = require("tiny-csrf");
 const app = express();
-const { Todo, User } = require("./models");
+const { Admin } = require("./models");
 const bodyParser = require("body-parser");
 var cookieParser = require("cookie-parser");
 
@@ -42,3 +42,99 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    (username, password, done) => {
+      Admin.findOne({ where: { email: username } })
+        .then(async function (user) {
+          const result = await bcrypt.compare(password, user.password);
+          if (result) {
+            return done(null, user);
+          } else {
+            return done(null, false, { message: "Invalid password" });
+          }
+        })
+        .catch((error) => {
+          return done(error);
+        });
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  console.log("Serialising user in session", user.id);
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  Admin.findByPk(id)
+    .then((user) => {
+      done(null, user);
+    })
+    .catch((error) => {
+      done(error, null);
+    });
+});
+
+app.use(function (request, response, next) {
+  response.locals.messages = request.flash();
+  next();
+});
+
+app.get("/", async (request, response) => {
+  response.render("index", {
+    title: "Voting Application",
+    csrfToken: request.csrfToken(),
+  });
+});
+
+app.get("/signup", async (request, response) => {
+  response.render("signup", {
+    title: "Signup",
+    csrfToken: request.csrfToken(),
+  });
+});
+
+app.post("/admin", async (request, response) => {
+  //I used trim() so that empty strings of any length are counted as 0 only
+  //even empty strings are not allowed.
+  //I put restrictions on firstname, email and password only
+  //Last name is optional.
+  if (request.body.firstName.trim().length === 0) {
+    request.flash("error", "First name is mandatory");
+    return response.redirect("/signup");
+  }
+  if (request.body.email.trim().length === 0) {
+    request.flash("error", "Email ID is a mandatory field");
+    return response.redirect("/signup");
+  }
+  if (request.body.password.length < 5) {
+    request.flash("error", "Password should be of atleast 5 characters");
+    return response.redirect("/signup");
+  }
+  const hashedPassword = await bcrypt.hash(request.body.password, saltRounds);
+  console.log(hashedPassword);
+  try {
+    const user = await Admin.create({
+      firstName: request.body.firstName,
+      lastName: request.body.lastName,
+      email: request.body.email,
+      password: hashedPassword,
+    });
+    request.login(user, (err) => {
+      if (err) {
+        console.log(err);
+      }
+      response.redirect("/todos");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports = app;
